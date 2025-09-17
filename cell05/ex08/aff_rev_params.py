f3d4c30d34f5cb74626d79f10d09655628a144ed
goatikun <67050616@kmitl.ac.th>
2025-09-17 19:11:35 +0700

    Create aff_rev_params.py

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,11 @@
+s = str(input("")) 
+words = s.split() 
+if s == " ":
+ print("none")
+elif len(words) < 2:
+  print("none")
+else:
+  i = len(words)-1
+  for a in range(0,len(words)):
+    print(words[i],end = " ")
+    i = i-1
