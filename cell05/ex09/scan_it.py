c852fae379cb70642eb8817ccd96b30c6db278d8
goatikun <67050616@kmitl.ac.th>
2025-09-17 19:13:58 +0700

    Create scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,11 @@
+a = input(" ").strip()
+b = input(" ").strip()
+words = b.split() 
+if a == " " or b == " ":
+ print("none")
+else:
+  count = 0
+  for i in words:
+    if i == a:
+        count = count+1
+  print(count)
