9c6d5ccc9e5ab237e15f32677c8df73a47f5540e
goatikun <67050616@kmitl.ac.th>
2025-09-17 22:34:48 +0700

    Update round_up.py

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,5 +1,4 @@
-i = float(input("Give me a number: "))
-if 0 < i < 1:
-  print(1)
-else:
-  print(round(i))
+import math
+num = float(input("Give me a number: "))
+rounded = math.ceil(num)
+print(rounded)
