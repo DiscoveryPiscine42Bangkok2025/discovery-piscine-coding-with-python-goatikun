70fcee7479c8869125d75fd95846e7a49ff2f67b
goatikun <67050616@kmitl.ac.th>
2025-09-17 19:16:42 +0700

    Create parameter_matching.py

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,9 @@
+a = str(input(" "))
+if a == " ":
+  print("none")
+else:
+  ans = str(input("What was the parameter? "))
+  if a == ans:
+    print("Good job!")
+  else:
+    print("Nope, sorry...")
