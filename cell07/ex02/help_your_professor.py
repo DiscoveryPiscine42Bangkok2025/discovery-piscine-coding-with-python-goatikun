eb264db36b54db832f7f4f2034a36725b99a481b
goatikun <67050616@kmitl.ac.th>
2025-09-17 19:57:12 +0700

    Create help_your_professor.py

---
+++ cell07/ex02/help_your_professor.py
@@ -0,0 +1,20 @@
+def average(a):
+  x = 0
+  y = len(a)
+  for i in a.values():
+    x = x+i
+  return x/y
+class_3B = {
+"marine": 18,
+"jean": 15,
+"coline": 8,
+"luc": 9
+}
+class_3C = {
+"quentin": 17,
+"julie": 15,
+"marc": 8,
+"stephanie": 13
+}
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
