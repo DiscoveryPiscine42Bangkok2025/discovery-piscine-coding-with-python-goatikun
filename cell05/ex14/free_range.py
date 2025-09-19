f2bf44168f0c46c1751c0360eda0339381beaea7
goatikun <67050616@kmitl.ac.th>
2025-09-19 11:36:00 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,7 +1,18 @@
-num1 = str(input(" "))
-num2 = str(input(" "))
-if num1.strip() == "" or num2.strip() == "":
+#!/usr/bin/env python3
+import sys
+args = sys.argv[1:]
+if len(args) < 2:
     print("none")
 else:
-  words = range(int(num1),int(num2)+1)
-  print(list(words))
+    num1 = args[0].strip()
+    num2 = args[1].strip()
+    if not num1 or not num2:
+        print("none")
+    else:
+        try:
+            start = int(num1)
+            end = int(num2)
+            words = range(start, end + 1)
+            print(list(words))
+        except ValueError:
+            print("none")
